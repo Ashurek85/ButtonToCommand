298d725513e4371a2c23dea3487244b08aa4f4b1
Ashurek85 <45046196+Ashurek85@users.noreply.github.com>
2019-08-31 15:58:53 +0200

    Add files via upload

---
+++ ButtonToCommand/Configuration.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+
+namespace ButtonToCommand
+{
+    public class Configuration
+    {
+        private const string DevideGuidKey = "DeviceGuid";
+        private const string ReaderDelayMsKey = "ReaderDelayMs";
+
+        public Guid DeviceGuid { get; }
+        public int ReaderDelayMs { get; }
+        public Dictionary<int, string> ButtonCommands { get; }
+        public bool ShowInfo { get; }
+
+        public Configuration(bool showInfo)
+        {
+            ShowInfo = showInfo;
+
+            // ReaderDelayMs
+            string readerDelayMs = ConfigurationManager.AppSettings[ReaderDelayMsKey];
+            int readerDelay = 150; // Default value
+            if (!int.TryParse(readerDelayMs, out readerDelay))
+                Console.WriteLine($"Error reading {ReaderDelayMsKey} key from configuration file; use {ReaderDelayMs}ms");
+            ReaderDelayMs = readerDelay;
+
+            // DeviceGuid
+            string configGuid = ConfigurationManager.AppSettings[DevideGuidKey];
+            Guid objetiveGuid;
+            if (!Guid.TryParse(configGuid, out objetiveGuid))
+                throw new Exception("DeviceGuid not set or invalid Guid");
+            DeviceGuid = objetiveGuid;
+
+            // ButtonComands
+            IEnumerable<string> configKeys = ConfigurationManager.AppSettings.AllKeys.Except(new string[] { DevideGuidKey, ReaderDelayMsKey });
+            ButtonCommands = new Dictionary<int, string>();
+            foreach (string configKey in configKeys)
+            {
+                int intValue;
+                if (int.TryParse(configKey, out intValue))
+                {
+                    if (ButtonCommands.ContainsKey(intValue))
+                        Console.Write($"appSettings key {configKey} duplicated, only works first entry");
+                    else
+                        ButtonCommands.Add(intValue, ConfigurationManager.AppSettings[configKey].ToString());
+                }
+                else
+                {
+                    Console.WriteLine($"appSettings key {configKey} ignored; is not integer button");
+                }
+            }
+        }
+    }
+}
---
+++ ButtonToCommand/Program.cs
@@ -0,0 +1,133 @@
+﻿using SharpDX.DirectInput;
+using System;
+using System.Threading;
+
+namespace ButtonToCommand
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            try
+            {
+                bool showInfo = false;
+                if (args.Length == 1)
+                {
+                    if (args[0] == "-info")
+                        showInfo = true;
+                    else
+                    {
+                        ShowUsage();
+                        return;
+                    }
+                }
+                else if (args.Length > 1)
+                {
+                    ShowUsage();                    
+                    return;
+                }
+
+                // Get configuration
+                Configuration config = new Configuration(showInfo);
+
+                // Initialize DirectInput
+                var directInput = new DirectInput();
+
+                // Find gamepad/joystick
+                bool found = false;
+                int devicesFoundCount = 0;
+                foreach (DeviceInstance device in directInput.GetDevices(DeviceType.Gamepad, DeviceEnumerationFlags.AllDevices))
+                {
+                    devicesFoundCount++;
+
+                    if (config.ShowInfo)
+                        Console.WriteLine("Gamepad: " + device.InstanceName + " " + device.InstanceGuid);
+                    if (!found && device.InstanceGuid == config.DeviceGuid)
+                        found = true;
+                }
+
+                foreach (DeviceInstance device in directInput.GetDevices(DeviceType.Joystick, DeviceEnumerationFlags.AllDevices))
+                {
+                    devicesFoundCount++;
+
+                    if (config.ShowInfo)
+                        Console.WriteLine("Joystick: " + device.InstanceName + " " + device.InstanceGuid);
+                    if (!found && device.InstanceGuid == config.DeviceGuid)
+                        found = true;
+                }
+
+                if (devicesFoundCount == 0)
+                    Console.WriteLine("No Gamepads/Joysticks (DirectInput) found");
+
+                if (!found)
+                {
+                    Console.WriteLine($"Gamepad/Joystick {config.DeviceGuid.ToString()} not found!");
+                    Console.ReadKey();
+                }
+                else
+                {
+                    // Reader loop
+                    ReadInputDevice(directInput, config);
+                }
+                
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        private static void ReadInputDevice(DirectInput directInput, Configuration config)
+        {
+            Joystick joystick = new Joystick(directInput, config.DeviceGuid);
+            joystick.Acquire();
+            while (true)
+            {
+                // Delay to save CPU usage
+                Thread.Sleep(config.ReaderDelayMs);
+                // Get joystick state
+                JoystickState state = joystick.GetCurrentState();
+
+                bool anyActive = false;
+                for (int i = 0; i < state.Buttons.Length; i++)
+                {
+                    if (state.Buttons[i])
+                    {
+                        Console.Write(i + " ");
+                        anyActive = true;
+                    }
+                }
+                if (anyActive)
+                    Console.WriteLine();
+
+                foreach (int button in config.ButtonCommands.Keys)
+                {
+                    if (state.Buttons[button])
+                    {
+                        ExecuteCommand(config.ButtonCommands[button]);
+                        if (config.ShowInfo)
+                            Console.WriteLine("Execute: " + config.ButtonCommands[button]);
+                    }
+                }
+            }
+        }
+
+        private static void ExecuteCommand(string command)
+        {
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = "/C " + command;
+            process.StartInfo = startInfo;
+            process.Start();
+        }
+
+        private static void ShowUsage()
+        {
+            Console.WriteLine("Usage: ButtonToCommand [-info]");
+            Console.ReadKey();
+        }
+    }
+}
